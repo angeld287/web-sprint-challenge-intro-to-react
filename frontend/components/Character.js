9a20542d7a2a8303081c926894a852d852be4803
Angel Angeles <daniel_1234@hotmail.es>
2023-10-07 12:18:12 -0400

    cleaning unused code

--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react'
+import React, { useCallback, useState } from 'react'
 
 function Character(props) { // ❗ Add the props
   // ❗ Create a state to hold whether the homeworld is rendering or not
@@ -7,9 +7,6 @@ function Character(props) { // ❗ Add the props
   // ❗ Create a "toggle" click handler to show or remove the homeworld
   const toggleShowHomeWorld = useCallback(() => setShowHomeWorld(!showHomeWorld), [showHomeWorld]);
 
-  useEffect(() => {
-    console.log(props.homeworld.name)
-  }, [])
   return (
     <div>
       {/* Use the same markup with the same attributes as in the mock */}
